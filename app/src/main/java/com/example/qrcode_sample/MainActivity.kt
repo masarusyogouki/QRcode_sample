0d6751c6aad1ec777068bd1a7177e52c4aaebae2
Yudai <105198285+masarusyogouki@users.noreply.github.com>
2024-03-18 15:16:35 +0900

    QRcodeスキャナー

--- app/src/main/java/com/example/qrcode_sample/MainActivity.kt
+++ app/src/main/java/com/example/qrcode_sample/MainActivity.kt
@@ -1,34 +1,120 @@
 package com.example.qrcode_sample
 
-import android.content.Intent
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
-import android.widget.Button
 import android.widget.Toast
 import androidx.activity.ComponentActivity
-import com.google.zxing.integration.android.IntentIntegrator
+import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
+import com.example.qrcode_sample.ui.theme.QRcode_sampleTheme
+import com.journeyapps.barcodescanner.ScanContract
+import com.journeyapps.barcodescanner.ScanOptions
 
 class MainActivity : ComponentActivity() {
+
+    private var textResult = mutableStateOf("")
+
+    private val barCodeLauncher = registerForActivityResult(ScanContract()) {
+        result ->
+        if (result.contents == null) {
+            Toast.makeText(this@MainActivity, "Cancelled", Toast.LENGTH_SHORT).show()
+        }
+        else {
+            textResult.value =result.contents
+        }
+    }
+    private fun showCamera(){
+        val options = ScanOptions()
+        options.setDesiredBarcodeFormats(ScanOptions.QR_CODE)
+        options.setPrompt("Scan a QR code")
+        options.setCameraId(0)
+        options.setBeepEnabled(false)
+        options.setOrientationLocked(false)
+
+        barCodeLauncher.launch(options)
+    }
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) {
+        isGrantted ->
+        if (isGrantted) {
+            showCamera()
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        var qrButton = findViewById<Button>(R.id.scanner)
-
-        // ボタンを押した時にQR読み取り画面に遷移する
-        qrButton.setOnClickListener{
-            IntentIntegrator(this).apply {
-                captureActivity=QRCodeCaptureActivity::class.java
-            }.initiateScan()
+        setContent {
+            QRcode_sampleTheme {
+                Scaffold(
+                    bottomBar = {
+                        BottomAppBar(
+                            actions = {},
+                            floatingActionButton = {
+                                FloatingActionButton(onClick = { checkCameraPermission(this@MainActivity) }) {
+                                    Icon(painter = painterResource(id = R.drawable.qrcode_scan),
+                                        contentDescription = "QR scan"
+                                    )
+                                }
+                            }
+                        )
+                    }
+                ) { innerPadding ->
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.qrcode_scan),
+                            modifier = Modifier.size(100.dp),
+                            contentDescription = "QR"
+                        )
+                        Text(
+                            text = textResult.value,
+                            fontSize = 30.sp,
+                            fontWeight = FontWeight.Bold
+                            )
+                    }
+                }
+            }
         }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        //読み取り結果
-        val result = IntentIntegrator.parseActivityResult(resultCode,data)
-        if (result.contents != null) {
-            Toast.makeText(this, result.contents, Toast.LENGTH_LONG).show()
+    private fun checkCameraPermission(context: Context) {
+        if (ContextCompat.checkSelfPermission(
+            context,
+            android.Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED) {
+            showCamera()
+        }
+        else if (shouldShowRequestPermissionRationale(android.Manifest.permission.CAMERA)) {
+            Toast.makeText(this@MainActivity, "Camera required", Toast.LENGTH_SHORT).show()
+        }
+        else {
+            requestPermissionLauncher.launch(android.Manifest.permission.CAMERA)
         }
     }
 }
